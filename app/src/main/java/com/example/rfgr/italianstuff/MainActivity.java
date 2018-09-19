f9a8b17f366decc94b17efa62a8526bb0b1ab03e
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-09-19 18:40:43 +0200

    Drobne poprawki.

--- app/src/main/java/com/example/rfgr/italianstuff/MainActivity.java
+++ app/src/main/java/com/example/rfgr/italianstuff/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rfgr.italianstuff;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -23,13 +24,21 @@ public class MainActivity extends Activity {
     private ListView drawerList;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private int currentPosition = 0;
+
+    //poniżej element nasłuchujący klknięcia w listę z szuflady
+    private class DrawerItemClickListner implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position); //po wybraniu elementu z listy zostanie wywołany kod metody selectItem
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().setDisplayHomeAsUpEnabled(true); //włączam przyciski "w górę"
-        getActionBar().setHomeButtonEnabled(true); //włączam przyciski "w górę"
+
         titles = getResources().getStringArray(R.array.titles); //odnajduję listę główną szuflady
         drawerList = (ListView) findViewById(R.id.drawer); //odnajduję sam widok szuflady
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); //odnajduję layout w którym znajduje się szuflada
@@ -37,16 +46,19 @@ public class MainActivity extends Activity {
                 android.R.layout.simple_list_item_activated_1, titles)); //a używając ...activated list item, elementy wybrane zostaną podświetlone
 
         drawerList.setOnItemClickListener(new DrawerItemClickListner()); //dodaję utworzon nasłuchiwacz do widoku listy
-
-        if (savedInstanceState == null) { //jeżeli zapisany stan aktywności wynosił nic to późniejsze metody też zostaną wykonane od niczego
-            selectItem(0);
+        if (savedInstanceState != null) { //jeżeli aktywność została usunięta, pasek tytułu otrzyma nazwę ostatniej aktywności
+            currentPosition = savedInstanceState.getInt("position");
+            setActionBarTitle(currentPosition);
+        } else {
+            selectItem(0); //jeżeli jednak aktywność nie miała wcześniej stanu wyświetlony zostanie domyślny tytuł- aktywności głównej
         }
+
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, //tworzę obiekt ActionBarDrawerToggle
                 R.string.open_drawer, R.string.close_drawer) {
             @Override
             public void onDrawerClosed(View view) { //metoda wywołana kiedy szuflada będzie zamknięta
                 super.onDrawerClosed(view);
-                invalidateOptionsMenu(); //metoda invalidateOptionsMenu informuje że należy otworzyć/użyć elementu menu
+                invalidateOptionsMenu(); //metoda invalidateOptionsMenu informuje że należy użyć elementu menu-otworzyć lub zamknąć szufladę
             }
 
             @Override
@@ -56,34 +68,30 @@ public class MainActivity extends Activity {
             }
         };
         drawerLayout.setDrawerListener(drawerToggle); //ustawiam ActionBarDrawerToggle jako obiekt nasłuchujący układu DrawerLayout
-    }
+        getActionBar().setDisplayHomeAsUpEnabled(true); //włączam przyciski "w górę"
+        getActionBar().setHomeButtonEnabled(true); //włączam przyciski "w górę"
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) { //metoda synchronizująca stan przycisku ActionBarDrawerToggle z stanem szuflady
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) { //metoda przekazuje wszelkie zmiany do przycisku ActionBarDrawerToggle
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) { //określam widoczność akcji, zaleznie czy szuflada jest otwarta czy zamknięta
-        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
-        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    //poniżej element nasłuchujący klknięcia w listę z szuflady
-    private class DrawerItemClickListner implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position); //po wybraniu elementu z listy zostanie wywołany kod metody selectItem
-        }
+        getFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            public void onBackStackChanged() { //metoda wywoływana przy zmianie stanu cofnięć
+                FragmentManager fragMan = getFragmentManager();
+                Fragment fragment = fragMan.findFragmentByTag("visible_fragment");
+                //sprawdzam który fragment jest aktualnie dołączony do aktywności, który aktualnie jest wyświetlany
+                if (fragment instanceof TopFragment) {
+                    currentPosition = 0;
+                }
+                if (fragment instanceof PizzaFragment) {
+                    currentPosition = 1;
+                }
+                if (fragment instanceof PastaFragment) {
+                    currentPosition = 2;
+                }
+                if (fragment instanceof StoresFragment) {
+                    currentPosition = 3;
+                }
+                setActionBarTitle(currentPosition); //ustawiam pasek tytułu zgodnie z numerem ostatniego fragmentu
+                drawerList.setItemChecked(currentPosition, true);
+            }
+        });
     }
 
     //poniższa metoda wykorzystuje swich by okreśłić który fragment ma zostać wciśnięty do ramki w ekranie główny by nie tworzyć dodatkowych widoków
@@ -110,6 +118,31 @@ public class MainActivity extends Activity {
         setActionBarTitle(position); //wykorzystam inną metodę by podmienić nazwę na pasku akcji
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); //na zakończenie odszukuję layout szuflady
         drawerLayout.closeDrawer(drawerList); //i go zamykam ustępując miejsca nowo wstawionemu fragmentowi
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) { //określam widoczność akcji, zaleznie czy szuflada jest otwarta czy zamknięta
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        menu.findItem(R.id.action_share).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) { //metoda synchronizująca stan przycisku ActionBarDrawerToggle z stanem szuflady
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) { //metoda przekazuje wszelkie zmiany do przycisku ActionBarDrawerToggle
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) { //metoda odpowiadająca za zapisanie stanu aktywności w przypadku jej zamknięcia,
+        super.onSaveInstanceState(outState); //zapisuje numer fragmentu w zmiennej currentPosition
+        outState.putInt("position", currentPosition);
     }
 
     private void setActionBarTitle(int position) { //metoda umieszczająca nazwę fragmentu w pasku akcji
